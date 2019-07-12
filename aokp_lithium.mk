6df53d4ff3fc39dd49485d26ce7dcc2a87aef77b
Nicholas Magill <nmagill1979@gmail.com>
2019-07-12 01:34:32 +0100

    AOKP-ify

--- aokp_lithium.mk
+++ aokp_lithium.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lithium/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_lithium
+PRODUCT_NAME := aokp_lithium
 PRODUCT_DEVICE := lithium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI MIX
